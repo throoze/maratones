7ff39be18b747cc7e3cc9809ba2ec729aa576e2d
Melecio <pontezambrano@gmail.com>
2012-10-27 12:39:23 -0430

    Arreglando errores de Mike (MP)

--- maratones/models.py
+++ maratones/models.py
@@ -21,15 +21,15 @@ class Pago(models.Model):
     cta = models.ForeignKey(CuentaBancaria)
 
 class Inscripcion(models.Model):
-	equipo = models.CharField(max_length=30)
-	solic_previa = models.OneToOneField(inscripcion)
-	
+    equipo = models.CharField(max_length=30)
+    solic_previa = models.OneToOneField(Solicitud)
+    
 class Patrocinante(models.Model):
-	nombre = models.CharField(max_length=30)
-	rif = models.CharField(max_length=20)
-	logo = models.ImageField(upload_to=)
-	# Se debe de poner la direccion de "upload" en el archivo settings.py
+    nombre = models.CharField(max_length=30)
+    rif = models.CharField(max_length=20)
+#    logo = models.ImageField(upload_to=)
+# Se debe de poner la direccion de "upload" en el archivo settings.py
 
 class Premiacion(models.Model):
-	nombre_premio = models.CharField(max_length=30)
-	posicion = models.CharField(max_length=30)
+    nombre_premio = models.CharField(max_length=30)
+    posicion = models.CharField(max_length=30)
