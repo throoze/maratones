405e69540770bc7349426afc473b73df33af4502
Victor De Ponte <rdbvictor19@gmail.com>
2012-10-27 17:17:14 -0430

    Todos los modelos registrados en el admin

--- maratones/admin.py
+++ maratones/admin.py
@@ -1,4 +1,12 @@
 from maratones.models import Solicitud, Inscripcion, Patrocinante, Premiacion, CuentaBancaria, Pago
 from django.contrib import admin
 
-admin.site.register(Solicitud, Inscripcion, Patrocinante, Premiacion, CuentaBancaria, Pago)
+
+# Modelos registrados en el admin
+
+admin.site.register(Solicitud)
+admin.site.register(Inscripcion)
+admin.site.register(Patrocinante)
+admin.site.register(Premiacion)
+admin.site.register(CuentaBancaria)
+admin.site.register(Pago)
